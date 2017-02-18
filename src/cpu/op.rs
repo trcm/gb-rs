cdcbc561078f2c406d6f9dcb42c4ad38d8c43843
Tom Midson <midson.trc@gmail.com>
2017-02-18 16:14:57 +1000

    Added the first few opcodes to the new Opcode struct
    The structure of this code is shamelessly copied from yupferris' Rustal-Boy code.

--- src/cpu/op.rs
+++ src/cpu/op.rs
@@ -9,23 +9,31 @@ pub struct Command {
 const OPCODE_DEC_B: u16 = 0x05;
 const OPCODE_LD_B: u16 = 0x06;
 const OPCODE_INC_C: u16 = 0x0C;
+const OPCODE_LD_DE: u16 = 0x17;
+
+
 
 
 pub enum Opcode {
     DecB,
     Ldb,
-    IncC
+    IncC,
+    Ldde,
 }
 
 impl Opcode {
     pub fn parse(bits: u16) -> Opcode {
         match bits {
-            
+            OPCODE_DEC_B => Opcode::DecB,
+            OPCODE_LD_B => Opcode::Ldb,
+            OPCODE_INC_C => Opcode::IncC,
+            OPCODE_Ldde => Opcode::Ldde,
         }
+
     }
 }
 
-impl fmt::Display for Opcoe {
+impl fmt::Display for Opcode {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let code = match self {
             &Opcode::DecB => "DEC B",
